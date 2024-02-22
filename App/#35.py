12b71bfd23bbbdefe0edeaa01dcbb4275859093d
Manuel Aguilera <158592921+maguilerafr@users.noreply.github.com>
2024-02-22 20:12:33 +0100

    Update #35.py

---
+++ App/#35.py
@@ -0,0 +1,23 @@
+class Dia:
+    def __init__(self):
+        self.horario_apertura = []
+
+    def dividir_en_medias_horas(self):
+        for hora in range(11, 24):
+            self.horario_apertura.append(f"{hora}:00 / {hora}:30")
+            self.horario_apertura.append(f"{hora}:30 / {hora+1}:00")
+
+    def obtener_horario_apertura(self):
+        return self.horario_apertura
+
+def main():
+    mi_dia = Dia()
+    mi_dia.dividir_en_medias_horas()
+    lista_horario_apertura = mi_dia.obtener_horario_apertura()
+    
+    print("\nHorario de apertura:")
+    for hora_apertura in lista_horario_apertura:
+        print(hora_apertura)
+
+if __name__ == "__main__":
+    main()
