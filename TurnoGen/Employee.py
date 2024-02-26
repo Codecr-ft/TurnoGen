6e511b429bd5babe5dd4f4a4657ea12ab11c6bf2
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-02-26 20:23:18 +0100

    Creo la clase empleado y le añado los atributos de tipo contrato y responsabilidad

---
+++ TurnoGen/Employee.py
@@ -0,0 +1,5 @@
+class Employee:
+    def __init__(self, contract_type, labor_responsibility, shift_management_ability):
+        self.contract_type = contract_type
+        self.labor_responsibility = labor_responsibility
+        self.shift_management_ability = shift_management_ability
--- TurnoGen/EmploymentContract.py
+++
@@ -1,3 +0,0 @@
-class EmploymentContract:
-    def __init__(self, contract_type):
-        self.contract_type = contract_type
