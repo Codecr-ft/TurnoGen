835b74bfd7a7af4d399f6d615d9e46c0b29ccd0a
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-02-26 20:07:48 +0100

    Añado las clases para responsabilidad empleado y contrato empleado

---
+++ TurnoGen/EmployeeResponsability.py
@@ -0,0 +1,4 @@
+class EmployeeResponsibility:
+    def __init__(self, labor_responsibility, shift_management_ability):
+        self.labor_responsibility = labor_responsibility
+        self.shift_management_ability = shift_management_ability
---
+++ TurnoGen/EmploymentContract.py
@@ -0,0 +1,3 @@
+class EmploymentContract:
+    def __init__(self, contract_type):
+        self.contract_type = contract_type
