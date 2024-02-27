1cff2976748d6e77b7d4a0a53432b5544b4a808d
Manuel Aguilera <158592921+maguilerafr@users.noreply.github.com>
2024-02-27 17:38:06 +0100

    Modelar un turno. Cambio del código para la MS0 #35.py
    El anterior código proporcionaba una herramienta que no era lo que se pedía en la MS0. Con este código se pretende crear una estructura que modele el concepto del turno con las tres variables principales: hora de entrada, hora de salida y tipo de turno.

--- App/#35.py
+++ App/#35.py
@@ -1,35 +1,16 @@
-from datetime import datetime, timedelta
+from datetime import time
 
-def get_workday(check_in, full_time=True):
-    try:
-        check_in = datetime.strptime(check_in, "%H:%M")
-    except ValueError:
-        print("Formato de hora incorrecto. Utiliza el formato HH:MM (por ejemplo, 09:00)")
-        return
+class Turno:
+    def __init__(self, check_in, type_workday):
+        self.check_in = check_in
+        self.type_workday = type_workday
+        self.check_out = self.get_check_out()
 
-    check_out = check_in + timedelta(hours=8 if full_time else 4)
-    
-    return check_in.strftime("%H:%M"), check_out.strftime("%H:%M")
-
-def main():
-    check_in = input("Introduce la hora de entrada (formato HH:MM): ")
-    type_journey = input("¿Es un turno de jornada completa? (s/n): ").lower()
-    
-    if type_journey == 's':
-        full_time = True
-    elif type_journey == 'n':
-        full_time = False
-    else:
-        print("Opción inválida. Por favor, responde 's' para sí o 'n' para no.")
-        return
-
-    check_in, check_out = get_workday(check_in, full_time)
-    
-    if check_in and check_out:
-        print("Rango de turno:")
-        print(f"Hora de entrada: {check_in}")
-        print(f"Hora de salida: {check_out}")
-
-if __name__ == "__main__":
-    main()
-
+    def get_check_out(self):
+        if self.type_workday == 'completa':
+            new_hour = (self.check_in.hour + 8)
+        elif self.type_workday == 'partida':
+            new_hour = (self.check_in.hour + 4)
+        else:
+            raise ValueError("Tipo de jornada no válido")
+        return time(new_hour, self.check_in.minute)
