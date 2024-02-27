736b90646ca5fd2118c951275ca12497e8fc0235
LugaresDev <lugaresdev@gmail.com>
2024-02-27 15:36:10 +0100

    feat#32_agregado_clase_Month_en_Month.py

---
+++ TurnoGen/Month.py
@@ -0,0 +1,12 @@
+class Month:
+    def __init__(self, name, num_days):
+        """
+        Constructor de la clase Month.
+
+        Args:
+        - name: El nombre del mes.
+        - num_days: El número total de días en el mes.
+        """
+        self.name = name
+        self.num_days = num_days
+        self.days = []
