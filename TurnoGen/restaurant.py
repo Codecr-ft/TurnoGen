7f4fda2a729ece2b689c27d92c283e08d80adb61
GLYEB_MAG <magazgleb@gmail.com>
2024-03-06 16:56:17 +0100

    Cambio de codigo para hacer la class completo

--- TurnoGen/restaurant.py
+++ TurnoGen/restaurant.py
@@ -1,3 +1,11 @@
-class Restaurant:  
+class Restaurant:
     def __init__(self, employees=None):
-        self.employees = employees or []  
+        self.employees = employees or []
+
+    def add_employee(self, employee):
+        self.employees.append(employee)
+
+    def remove_employee(self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+ 
