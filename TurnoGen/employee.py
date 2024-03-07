50abc61801f472e56617bb138661068b4d99e8ad
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 19:47:11 +0100

    Creamos la lista de empleados

--- TurnoGen/employee.py
+++ TurnoGen/employee.py
@@ -4,3 +4,13 @@ class Employee:
         self.labor_responsibility = labor_responsibility
         self.shift_management_ability = shift_management_ability
         self.email = email
+        
+    def create_default_employees(self):
+        employees = [
+            Employee("full-time", "waiter", True, "d'artgnan@example.com"),
+            Employee("part-time", "waiter", False, "aramis@example.com"),
+            Employee("full-time", "waiter", True, "athos@example.com"),
+            Employee("part-time", "waiter", False, "portos@example.com"),
+            Employee("full-time", "waiter", True, "alexanderdumas@example.com")
+        ]
+        return employees
