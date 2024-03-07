f3ff0c8b41c33151c3fb209eb42ce30f4a476f96
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 17:25:07 +0100

    Update de la clase restaurant

--- TurnoGen/restaurant.py
+++ TurnoGen/restaurant.py
@@ -1,41 +1,19 @@
-from employee import Employee  # Importar la clase Employee desde el archivo employee.py
+from business_hours import BusinessHours
 
 class Restaurant:
-    def __init__(self):
-        """
-        Constructor de la clase Restaurant.
-        """
+    def __init__(self, name, business_hours):
+        self.name = name
+        self.business_hours = business_hours
         self.employees = []
+        self.shifts = []
 
     def add_employee(self, employee):
-        """
-        Agrega un nuevo empleado al restaurante.
-        
-        Args:
-        - employee: Objeto Employee a agregar.
-        """
+       
         self.employees.append(employee)
 
-    def remove_employee(self, employee):
-        """
-        Elimina un empleado del restaurante.
-        
-        Args:
-        - employee: El objeto Employee a eliminar.
-        """
-        self.employees = [e for e in self.employees if not e == employee]
+    def assign_shift(self, employee_id, shift):
+       
+        pass
 
-    def update_employee(self, old_employee, new_employee):
-        """
-        Actualiza la información de un empleado en la lista de empleados del restaurante.
-        
-        Args:
-        - old_employee: El objeto Employee cuya información se va a actualizar.
-        - new_employee: El nuevo objeto Employee con la información actualizada.
-        """
-        for i, e in enumerate(self.employees):
-            if e == old_employee:
-                self.employees[i] = new_employee
-                print(f"Empleado actualizado: {old_employee} -> {new_employee}")
-                return
-        print(f"{old_employee} no se encontró en la lista de empleados.")
+    def set_business_hours(self, opening_time, closing_time):
+        self.business_hours = BusinessHours(opening_time, closing_time)
