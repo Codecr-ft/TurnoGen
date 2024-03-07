0804d174882d7f06260eac954935a1dc554d17d7
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 17:49:16 +0100

    update de la clase restaurant
    Borro todas las funciones

--- TurnoGen/restaurant.py
+++ TurnoGen/restaurant.py
@@ -1,19 +1,7 @@
-from business_hours import BusinessHours
+
 
 class Restaurant:
-    def __init__(self, name, business_hours):
-        self.name = name
+    def __init__(self, business_hours):
         self.business_hours = business_hours
         self.employees = []
         self.shifts = []
-
-    def add_employee(self, employee):
-       
-        self.employees.append(employee)
-
-    def assign_shift(self, employee_id, shift):
-       
-        pass
-
-    def set_business_hours(self, opening_time, closing_time):
-        self.business_hours = BusinessHours(opening_time, closing_time)
