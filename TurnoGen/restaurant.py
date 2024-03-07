1d5a8c608847901febd25f0a69649e2abdc5603a
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 18:59:32 +0100

    Update de restaurant
    Añadimos nombre de restaurante, turnos y lista de empleados

--- TurnoGen/restaurant.py
+++ TurnoGen/restaurant.py
@@ -1,7 +1,6 @@
-
-
 class Restaurant:
-    def __init__(self, business_hours):
-        self.business_hours = business_hours
-        self.employees = []
-        self.shifts = []
+    def __init__(self, business_hours, name = "Carmelita"):
+        self.name = name 
+        self.business_hours = business_hours  
+        self.employees = []  ## Por ahora sin empleados, estructura lista para añadir
+        self.shifts = {'completo': [], 'partido': []}
