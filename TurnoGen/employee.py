fb5e57b766aeeecedb2aa98a19ba3b3780dd7564
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 20:33:55 +0100

    closes #53 borro ejemplos de la lista de empleados

--- TurnoGen/employee.py
+++ TurnoGen/employee.py
@@ -24,11 +24,6 @@ class Employee:
         self.shift_management_ability = shift_management_ability
         self.email = email
         
-        employees = [
-            Employee("full-time", "waiter", True, "d'artgnan@example.com"),
-            Employee("part-time", "waiter", False, "aramis@example.com"),
-            Employee("full-time", "waiter", True, "athos@example.com"),
-            Employee("part-time", "waiter", False, "portos@example.com"),
-            Employee("full-time", "waiter", True, "alexanderdumas@example.com")
-        ]
+        employees = []
+        
 
