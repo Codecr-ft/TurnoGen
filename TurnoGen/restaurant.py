b3675edb4c867a55ff65dcea4ef526f81848a5ca
Lolic3 <152490844+Lolic3@users.noreply.github.com>
2024-03-07 19:52:11 +0100

    Update

--- TurnoGen/restaurant.py
+++ TurnoGen/restaurant.py
@@ -1,6 +1,6 @@
 class Restaurant:
-    def __init__(self, business_hours, name = "Carmelita"):
-        self.name = name 
+    def __init__(self, business_hours, name):
+        self.name = name
         self.business_hours = business_hours  
         self.employees = []  ## Por ahora sin empleados, estructura lista para añadir
-        self.shifts = {'completo': [], 'partido': []}
+        self.shifts = {'completo': [], 'partido': []} 
